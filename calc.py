7b06bf4a1ae1bb095b06be4c0132759e7bdd812b
Geshi1507 <yurishink96@gmail.com>
2025-01-28 23:11:10 +0700

    p

--- calc.py
+++ calc.py
@@ -1,19 +1,9 @@
-def main():
-    # Запрос ввода от пользователя
-    input_expression = input("Input: ")
+def main(user_input: str) -> str:
 
-    # Разделение строки на части
-    parts = input_expression.split()
-
-    # Проверка на правильное количество элементов
-    if len(parts) == 1:
-        raise ValueError("Строка не является математической операцией")
-    if len(parts) >= 4:
-        raise ValueError("Формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)")
+    parts = user_input.split()
     if len(parts) != 3:
         raise ValueError("Ошибка")
 
-    # Парсинг чисел и операции
     try:
         a = int(parts[0])
         operator = parts[1]
@@ -21,11 +11,9 @@ def main():
     except ValueError:
         raise ValueError("Вводите целые числа от 1 до 10")
 
-    # Проверка диапазона для a и b
     if not (1 <= a <= 10) or not (1 <= b <= 10):
         raise ValueError("Числа должны быть от 1 до 10 включительно.")
 
-    # Выполнение операции
     if operator == '+':
         result = a + b
     elif operator == '-':
@@ -36,15 +24,17 @@ def main():
         if b == 0:
             raise ZeroDivisionError("Деление на ноль.")
         result = a // b
+
     else:
         raise ValueError("Неправильная операция. Используйте +, -, * или /.")
+    return str(result)
 
-    # Вывод результата
-    print(f"Output: {result}")
 
 
 if __name__ == "__main__":
     try:
-        main()
+        user_input = input("Input: ")
+        result = main(user_input)
+        print("Output:", result)
     except Exception as e:
         print(f"Ошибка: {e}")
